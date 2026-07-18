1601e9526b412521079f90f379aa9e18a505b872
Guglielmo Marengo <guglielmomare@gmail.com>
2026-07-18 13:13:18 +0200

    Refactor input validation into reusable method

--- src/genome.py
+++ src/genome.py
@@ -1,10 +1,8 @@
 class Genome:
-    def __init__(self, organism, chromosome ,sequence):
-
-        if type(sequence) != str:
-            raise TypeError("Sequence must be a string.")
-        if len(sequence) == 0:
-            raise ValueError("Sequence cannot be empty.")
+    def __init__(self, organism, chromosome , sequence):
+        self.validate_string("Organism", organism)
+        self.validate_string("Chromosome", chromosome)
+        self.validate_string("Sequence", sequence)
 
         sequence = sequence.upper()
         valid_nucleotides = {"A", "C", "G", "T"}
@@ -13,19 +11,16 @@ class Genome:
             if character not in valid_nucleotides:
                 raise ValueError(f"Invalid character {character} in sequence at position {i}.")
 
-        if type(organism) != str:
-            raise TypeError("Organism must be a string.")
-        if len(organism) == 0:
-            raise ValueError("Organism cannot be empty.")
-
-        if type(chromosome) != str:
-            raise TypeError("Chromosome must be a string.")
-        if len(chromosome) == 0:
-            raise ValueError("Chromosome cannot be empty.")
-
         self.sequence = sequence
         self.organism = organism
         self.chromosome = chromosome
+
+    @staticmethod
+    def validate_string(name, value):
+        if type(value) != str:
+            raise TypeError(f"{name} must be a string.")
+        if len(value) == 0:
+            raise ValueError(f"{name} cannot be empty.")
 
     def length(self):
         return len(self.sequence)
